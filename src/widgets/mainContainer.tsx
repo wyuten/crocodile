56d203186a2016b66027c82f4bac2d9c3905377f
Mike <nux7jr@gmail.com>
2023-09-16 18:38:23 +0700

    сделал по FSD

--- src/widgets/mainContainer.tsx
+++ src/widgets/mainContainer.tsx
@@ -1,24 +1,24 @@
 import RefWord from '../components/RefWord/ui/RefWord.js'
 import { useState, useEffect } from 'react'
-import { ListWords } from '../entities/words.js'
-import { randomizeWords } from '../features/randomizeWords.js'
+import { ListWords } from '../entities/words/model/words.js'
+import { randomizeWords } from '../features/randomizeWords/lib/randomizeWords.js'
 
+
+const randomizedArray = randomizeWords(ListWords);
 
 const useMainContainer = () => {
     const [data, setData] = useState("");
     const [count, setCount] = useState(0);
-    const randomizedArray = randomizeWords(ListWords);
     function updateWord(count: number) {
       setCount(count++)
+      console.log(count)
       setData(randomizedArray[count]);
     }
-    useEffect(() => {
-      setData(randomizedArray[0]);
-    }, [randomizedArray]);
     return (
       <>
     <RefWord word={data} />
         {/* это только для теста */}
+        {count}
         <button onClick={() => updateWord(count)}>next word</button>
       </>
     )
